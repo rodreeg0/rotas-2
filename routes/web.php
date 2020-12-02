372e722fc3d5210a770290bc675837b934702801
roodrigato <rodrigohaering333@gmail.com>
2020-12-01 23:57:21 -0300

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-// Route::get('/atividade4/{q1?}/{q2?}/{q3?}/{q4?}/{q5?}/{q6?}/{q7?}/{q8?}/{q9?}/{q10?}', function ($q1=null,$q2=null,$q3=null,$q4=null,$q5=null,$q6=null,$q7=null,$q8=null,$q9=null,$q10=null) {
-//     //Os parametros opcionais dessa rota são só para o link de teste que o prof pediu. Pois não da pra fazer POST só com um link.
-//     $data = array(
-//         '1' => $q1,
-//         '2' => $q2,
-//         '3' => $q3,
-//         '4' => $q4,
-//         '5' => $q5,
-//         '6' => $q6,
-//         '7' => $q7,
-//         '8' => $q7,
-//         '9' => $q9,
-//         '10' => $q10
-//     );
-//     return view('form')->with(compact('data'));
-// });
 
 Route::get('/atividade4', function () {
     return view('exercicio4');
